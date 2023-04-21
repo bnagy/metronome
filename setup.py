11bc38f4d5bfb425686ad7eb48503451ca65a9e1
Ben Nagy <ben.nagy.web@gmail.com>
2023-04-21 09:36:45 +0200

    add sampling methods

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ import os
 
 setup(
     name="Metronome",
-    version="0.0.2",
+    version="0.0.3",
     author="Ben Nagy",
     packages=["metronome"],
     license="3-Clause BSD",
     url="https://github.com/bnagy/metronome",
     long_description_content_type="text/markdown",
     long_description=open("README.md").read(),
-    install_requires=["setuptools", "pandas", "Bio", "ray"],
+    install_requires=["setuptools", "pandas", "numpy", "Bio", "ray"],
 )
